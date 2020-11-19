2abdf1a619f5eabddd92ee0585328edb4dc5672c
domison <dominikwschmitt@gmail.com>
2020-11-19 10:32:58 +0100

    Set new file structure

--- dist/Algorithm.js
+++
@@ -1,9 +0,0 @@
-export default class Algorithm {
-	constructor() {
-		this.current = [[2, 3], [3]]; // standard rule-set
-	}
-	// lives 2,3 - revives 3 - dies
-	setRule(rule = [[2, 3], [3]]) {
-		this.current = rule;
-	}
-}
